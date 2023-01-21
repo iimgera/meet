d130493303619abacbc4dfbe33c1f88f40f61fe4
iimgera <iimgera28062004@gmail.com>
2023-01-21 19:54:57 +0600

    first commit

--- meet/profile_app/models.py
+++ meet/profile_app/models.py
@@ -18,8 +18,8 @@ GENDER_CHOICES = [
 
 class Questionnaire(models.Model):
     nickname = models.CharField(max_length=20, blank=True, verbose_name='Nickname')
-    name = models.CharField(max_length=20, blank=True, null=True verbose_name='Name')
-    surname = models.CharField(max_length=20, blank=True, null=True verbose_name='Surname')
+    name = models.CharField(max_length=20, blank=True, verbose_name='Name')
+    surname = models.CharField(max_length=20, blank=True, null=True, verbose_name='Surname' )
     birthday = models.DateField(auto_now = False , auto_now_add = False )
     orientation = models.CharField(choices=ORIENTATION_CHOICES)
     description = models.TextField(blank=True, null=True)
@@ -27,9 +27,9 @@ class Questionnaire(models.Model):
     phone = models.IntegerField(blank=True, max_length=20)
     user = models.OneToOneField('User', related_name='questionnaire')
     
-class User(models.Model):
+# class User(models.Model):
 
 
-class Photo(models.Model):
-    image = models.ImageField(upload_to = None , height_field = None , width_field = None , max_length = 100)
-    questionnaire = 
+# class Photo(models.Model):
+#     image = models.ImageField(upload_to = None , height_field = None , width_field = None , max_length = 100)
+   
